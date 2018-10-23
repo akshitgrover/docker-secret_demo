0cf76788eb7941f234127cf23d2fc7e029aa5eec
Akshit Grover <akshit.grover2016@gmail.com>
2018-10-23 12:48:29 +0530

    Add index.js application file

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+
+const secret_1_data = fs.readFileSync("/run/secrets/SECRET_1");
+
+const secret_2_data = fs.readFileSync("/run/secrets/SECRET_2");
+
+const server = http.createServer();
+
+server.on("request", (req, res)=>{
+
+    res.setHeader("Content-Type", "application/json");
+    let body = {secret_1_data, secret_2_data};
+    let jsonBody = JSON.stringify(body);
+    res.write(jsonBody);
+    res.end();
+
+});
+
+server.listen(3000, (err)=>{
+
+    if(err){
+        console.log("Error lifting on port 3000");
+        process.exit(1);
+    }
+    console.log("Lifted on port 3000");
+
+});
